71d8c593d44295875d987391d6a090aa39699e7b
amalp12 <44752430+amalp12@users.noreply.github.com>
2020-06-12 09:59:14 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -6,19 +6,6 @@ master.geometry('1000x1000')
 master.title('Tic Tac Toe Game')
 
 
-# if __name__ ==  "__main__":
-    
-#     #playWithFriend()
-#     #playWithComputer()
-
-#     play_again = True
-
-#     while play_again ==True:
-
-
-#         master.mainloop()
-
-
 
 startHumanBtn = tk.Button( master, text = 'Play With a Friend', command = lambda :playWithFriend(master)) 
 startComputerBtn = tk.Button(master, text = 'Play With Computer', command = lambda : playWithComputer(master))
